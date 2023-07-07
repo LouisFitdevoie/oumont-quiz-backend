f1971a68b67753198cb8478a97121d827c688fbb
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2023-07-07 21:45:32 +0200

    Adding the Question model

---
+++ src/model/Question.js
@@ -0,0 +1,39 @@
+const uuid = require("uuid");
+
+class Question {
+  // Create a question from a line of the question file
+  constructor(fileLine) {
+    const line = fileLine.split(";");
+    this.id = uuid.v4();
+    this.questionType = line[0];
+    this.theme = line[1];
+    this.question = line[2];
+    this.answer = line[3];
+    this.points = parseInt(line[4]);
+    this.choices = line[5].split("/");
+    this.explanation = line[6];
+    this.isBonus = new Boolean(line[7]);
+  }
+}
+
+module.exports = Question;
+
+/* Function to read data from a csv file formatted like specified in the README.md
+
+const fs = require("fs");
+const Question = require("./model/Question");
+
+var questions = [];
+
+fs.readFile("./src/test.csv", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const fileContent = data.split("\n").slice(1);
+  fileContent.forEach((line) => {
+    questions.push(new Question(line));
+  });
+});
+
+*/
