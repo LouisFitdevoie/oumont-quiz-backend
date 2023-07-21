f8e8a1d58a8d6f9b12598b791bd8a61dcb301745
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2023-07-21 13:31:54 +0200

    Code to start the server + testing + updating package

--- src/main.js
+++ src/main.js
@@ -1,36 +1,57 @@
 require("dotenv").config();
 const mysql = require("mysql");
+const express = require("express");
 
-const databasePort = process.env.DATABASE_PORT || 3306;
+exports.startServer = () => {
+  const app = express();
 
-let db = {
-  database: "",
-  user: "",
-  password: "",
+  const databasePort = process.env.DATABASE_PORT || 3306;
+
+  let db = {
+    database: "",
+    user: "",
+    password: "",
+  };
+  if (process.env.NODE_ENV == "production") {
+    db.database = process.env.DATABASE_PROD;
+    db.user = process.env.DB_USER_PROD;
+    db.password = process.env.DB_PASSWORD_PROD;
+  } else if (process.env.NODE_ENV == "development") {
+    db.database = process.env.DATABASE_DEV;
+    db.user = process.env.DB_USER_DEV;
+    db.password = process.env.DB_PASSWORD_DEV;
+  } else if (process.env.NODE_ENV == "testing") {
+    db.database = process.env.DATABASE_TEST;
+    db.user = process.env.DB_USER_TEST;
+    db.password = process.env.DB_PASSWORD_TEST;
+  }
+  const baseURL = `/api/${process.env.API_VERSION}`;
+
+  const db_connection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: db.user,
+    password: db.password,
+    database: db.database,
+    port: databasePort,
+  });
+
+  db_connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`Connected to the database on port ${databasePort}!`);
+  });
+
+  const apiPort = process.env.API_PORT || 8000;
+
+  app.listen(apiPort, () => {
+    console.log(`Server started on port ${apiPort}`);
+  });
+
+  app.use(express.json());
+
+  app.get(baseURL + "/", (req, res) => {
+    res.json({
+      message: "Hello API World!",
+      status: 200,
+    });
+  });
 };
-if (process.env.NODE_ENV == "production") {
-  db.database = process.env.DATABASE_PROD;
-  db.user = process.env.DB_USER_PROD;
-  db.password = process.env.DB_PASSWORD_PROD;
-} else if (process.env.NODE_ENV == "development") {
-  db.database = process.env.DATABASE_DEV;
-  db.user = process.env.DB_USER_DEV;
-  db.password = process.env.DB_PASSWORD_DEV;
-} else if (process.env.NODE_ENV == "testing") {
-  db.database = process.env.DATABASE_TEST;
-  db.user = process.env.DB_USER_TEST;
-  db.password = process.env.DB_PASSWORD_TEST;
-}
-
-const db_connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: db.user,
-  password: db.password,
-  database: db.database,
-  port: databasePort,
-});
-
-db_connection.connect(function (err) {
-  if (err) throw err;
-  console.log(`Connected to the database on port ${databasePort}!`);
-});
